ceaa5d988d6bd3edbfc2b40150bff3a164d1bb96
Michael Ng <43243867+mjng93@users.noreply.github.com>
2020-04-13 10:29:08 -0500

    Delete google_analytics_covid1.js

--- google_analytics_covid1.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-161820817-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-161820817-1');
-</script>
